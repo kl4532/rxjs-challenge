d9682fac3c8c006f3a7b690568b3ff65f3d6815a
kl4532 <kornellukasiak@gmail.com>
2021-07-19 09:52:41 +0200

    unsub

--- src/app/challenges/c1/c1.component.ts
+++ src/app/challenges/c1/c1.component.ts
@@ -1,25 +1,30 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
-import {fromEvent} from "rxjs";
+import {Component, ElementRef, OnDestroy, OnInit} from '@angular/core';
+import {fromEvent, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-c1',
   templateUrl: './c1.component.html',
   styleUrls: ['./c1.component.scss']
 })
-export class C1Component implements OnInit {
+export class C1Component implements OnInit, OnDestroy {
 
   focusedElement: HTMLElement | undefined;
+  focusSub: Subscription | undefined;
 
   constructor(private el: ElementRef) { }
 
   ngOnInit(): void {
     const block = this.el.nativeElement.querySelector('.block-to-track');
-    const focusObservable = fromEvent(block, 'focusin').subscribe(
+    this.focusSub = fromEvent(block, 'focusin').subscribe(
       (focus: any) => {
-        this.focusedElement = focus.target;
+        this.focusedElement = focus.target.tagName;
       },
       (err)=>err,
       ()=>console.log('complete'));
+  }
+
+  ngOnDestroy() {
+    this.focusSub?.unsubscribe();
   }
 
 }
