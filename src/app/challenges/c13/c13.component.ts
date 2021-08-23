0d93c7dfa7331b207a7a10b8bd7bb7a303948556
kl4532 <kornellukasiak@gmail.com>
2021-08-23 12:52:53 +0200

    add scaling

--- src/app/challenges/c13/c13.component.ts
+++ src/app/challenges/c13/c13.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {fromEvent} from "rxjs";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-c13',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./c13.component.scss']
 })
 export class C13Component implements OnInit {
+  private scale=1;
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
+  get height() {
+    return this.scale * 100;
+  }
   ngOnInit(): void {
+    const banner = <HTMLElement>this.el.nativeElement.querySelector('.banner');
+    const scaleContainer = <HTMLElement>this.el.nativeElement.querySelector('.scale-container');
+
+    fromEvent(document, 'scroll').pipe(
+      tap(()=>{
+        let scale = scaleContainer.getBoundingClientRect().width / scaleContainer.offsetWidth;
+        scale = +scale.toFixed(2);
+        if(banner.offsetTop > 70) {
+          if(scale >= 0.6) {
+            scale = scale - 0.05;
+            scaleContainer.style.transform = `scale(${scale})`;
+          }
+        } else {
+          if(scale < 1) {
+            scale = scale + 0.05;
+            scaleContainer.style.transform = `scale(${scale})`;
+          }
+        }
+        this.scale = scale;
+      })
+    ).subscribe();
   }
 
 }
